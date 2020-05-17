9dffb9d8d7f54feee1df6702e0f1e17cd574c0d6
Sujeesh <sujeeshdl@gmail.com>
2020-05-17 13:25:11 +0100

    Loader added

--- src/app/shared-service/components/loader/loader.component.ts
+++ src/app/shared-service/components/loader/loader.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { loaderState } from './../../../store';
+import { IAppState } from './../../models';
 
 @Component({
   selector: 'app-loader',
@@ -6,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
+  loader = this.store.select(loaderState);
 
-  constructor() { }
+  constructor(private store: Store<IAppState>) {
+
+    // this.loader = this.store.select(loaderState);
+
+    this.loader.subscribe(data => console.log(data.message));
+
+  }
+
 
   ngOnInit(): void {
   }
